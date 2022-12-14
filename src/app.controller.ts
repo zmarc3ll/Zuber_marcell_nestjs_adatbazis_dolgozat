34b51a69ae633c471f9cf554404c5062cb25a8db
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-12-14 13:58:54 +0100

    app controller working

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,9 @@ import {
   Get,
   Render,
   Query,
+  Post,
+  Redirect,
+  Body,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
@@ -15,17 +18,19 @@ export class AppController {
   @Get()
   @Render('index')
   async index(@Query('szem_szin') szem_szin: string) {
-
     if (szem_szin != undefined) {
+
       const [ rows ] = await db.execute(
         'SELECT szem_szin, suly FROM macskak WHERE szem_szin = ?',
         [szem_szin]
         );
-  
+
       return {
         macskak: rows
       };
+
     }else{
+
       const [ rows ] = await db.execute(
         'SELECT szem_szin, suly FROM macskak ORDER BY suly DESC'
         );
@@ -33,6 +38,25 @@ export class AppController {
         return {
           macskak: rows
         };
+    } 
+  }
+  @Get('cats/new')
+  @Render('form')
+  newMacskaForm(){
+    return {};
+  }
+
+  @Post('cats/new')
+  @Redirect()
+  async newPainting(@Body() macskak: MacskaDto){
+
+    const [res]: any = await db.execute(
+      'INSERT INTO macskak (szem_szin, suly) VALUES (?, ?)',
+      [ macskak.szem_szin, macskak.suly ]
+    );
+
+    return{
+      url: '/',
     }
   }
 }
